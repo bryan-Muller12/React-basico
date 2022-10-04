64940f4721104c2eef85d64471d4a597e9443b0c
Bryan Muller <brysn.ms@gmail.com>
2022-10-04 11:53:16 -0300

    Códigos da aula 11

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,29 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import { useEffect, useInsertionEffect, useState } from "react";
-import Button from "../componentes/Button";
 
 const Home: NextPage = () => {
   return (
     <main className="flex items-center justify-center w-screen h-screen bg-gradient-to-br from-purple-400 to-purple-600">
-      <section className="flex flex-col items-center justify-evenly  gap-4 p-8 rounded shadow bg-white">
-        <p className="">Olha uma pagina</p>
-        <Link href="alt"  >
-            <a className="text-xl font-bold bg-purple-600 text-white p-4 rounded shadow hover:bg-purple-700 focus:outline-none focus:bg-purple-700 active:bg-purple-900">
-              Manda pra outra
-            </a>
-        </Link>
+      <section className="flex flex-col items-center justify-evenly max-w-xs gap-4 p-8 rounded shadow bg-white">
+        
+        <div className="w-full flex flex-col gap-4">
+          <h1>Aqui, temos uma imagem</h1>
+
+          <picture>
+              <img src="/img/pato.jpg" alt="imagem de um Pato" />
+          </picture>
+        </div>
+
+        <div className="w-full flex flex-col gap-4">
+          <h1>Aqui, temos uma imagem</h1>
+
+          <image>
+              <img src="/img/ganso.jpg" alt="imagem de um Pato" />
+          </image>
+        </div>
+          
+       
       </section>
     </main>
   );
